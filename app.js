c32ced5f1391dd4b6957bb06db663f0dc4163001
Kevin Bluer <kevin@bluer.com>
2012-05-16 13:37:37 -0400

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+// for express
+var express = require('express');
+
+// mailer
+var email = require("mailer");
+
+// create the express instance
+var app = module.exports = express.createServer();
+
+// configuration
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: "axsmap session" }))
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+// ******************
+// register endpoint
+// ******************
+
+app.get('/', function(request, response) {
+
+	response.send('hello world');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Scaffold is Listening on " + port);
+});
